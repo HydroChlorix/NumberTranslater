08ee8e3d53122208cb6a0e0bc293879ef3d33ba6
HydroChlorix <gold_exp@hotmail.com>
2017-09-11 23:26:22 +0700

    namespace

--- ConsoleApplication/V3.Test/TranslateTest.cs
+++ ConsoleApplication/V3.Test/TranslateTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
-namespace V3.Test
+namespace ConsoleTranslater.Test
 {
     [TestClass]
     public class TranslateTest
--- ConsoleApplication/V3/Program.cs
+++ ConsoleApplication/V3/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace V3
+namespace ConsoleTranslater
 {
     class Program
     {
